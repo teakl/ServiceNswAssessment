085e503e798e68a8e2484cbb940f5a01127a003f
Teak Ly <teakly@Teaks-MacBook-Air.local>
2026-07-02 21:26:08 +1000

    fixed hooks and steps

--- src/step-definitions/hooks/hooks.ts
+++ src/step-definitions/hooks/hooks.ts
@@ -5,24 +5,24 @@ import { pageFixture } from "./browserContextFixture";
 let browser: Browser;
 
 //BeforeAll hook: Runs once before all scenarios
-BeforeAll(async function(){
+BeforeAll(async () => {
     console.log("\nExecuting test suite...");
 })
 
 //AfterAll hook: Runs once after all scenarios
-AfterAll(async function(){
+AfterAll(async () => {
     console.log("\nFinished execution of test suite!");
 })
 
 // Before hook: Runs before each scenario
-Before(async function() {
+Before(async ()=> {
     browser = await chromium.launch({ headless: true });
-    pageFixture.context = await browser.newContext({ viewport: { width: 1280, height: 720 } });
+    pageFixture.context = await browser.newContext({ viewport: { width: 1440, height: 900 } });
     pageFixture.page = await pageFixture.context.newPage();
 })
 
 // After hook: Runs after each scenario
-After(async function() {
+After(async ()=> {
     if (pageFixture.page) {
         await pageFixture.page.close();
     }
